fb6d326ae23f5433b9d11216eaa4f839190f9a92
unknown <leandrochazarreta96@gmail.com>
2020-06-29 19:40:10 -0300

    menu

--- app/model/HomeModel.php
+++ app/model/HomeModel.php
@@ -15,12 +15,10 @@ class HomeModel
         // Arma el menu
         Switch ($permiso) {
             case '3':
-                $menu = array('Coronavirus' => 'Coronavirus', 'Policiales' => 'Policiales', 'Politica' => 'Politica', 'Mundo' => 'Mundo',
-                    'Sociedad' => 'Sociedad', 'Ciencia' => 'Ciencia', 'noticia' => 'Crear Noticia');
+                $menu = array('link1' => 'publicacion', 'noticia' => 'Crear Noticia');
                 break;
             case '4':
-                $menu = array('Coronavirus' => 'Coronavirus', 'Policiales' => 'Policiales', 'Politica' => 'Politica', 'Mundo' => 'Mundo',
-                    'Sociedad' => 'Sociedad', 'Ciencia' => 'Ciencia', 'noticia' => 'Crear Noticia', 'administrador' => 'Administrar Usuario');
+                $menu = array('link1' => 'publicacion', 'noticia' => 'Crear Noticia', 'administrador' => 'Administrar Usuario');
                 break;
             default:
                 $menu = array( 'Coronavirus' => 'Coronavirus', 'Policiales' => 'Policiales', 'Politica' => 'Politica', 'Mundo' => 'Mundo',
