e25054367e1e4bea456caa2128104b5a23a2ee72
unknown <leandrochazarreta96@gmail.com>
2020-07-02 21:47:16 -0300

    Menu arreglado

--- app/model/HomeModel.php
+++ app/model/HomeModel.php
@@ -15,15 +15,23 @@ class HomeModel
         // Arma el menu
         Switch ($permiso) {
             case '3':
-                $menu = array('link1' => 'crearPublicacion', 'noticia' => 'Crear Noticia');
+                $menu = array('Coronavirus' => 'Coronavirus', 'Policiales' => 'Policiales',
+                    'Politica' => 'Politica', 'Mundo' => 'Mundo', 'Sociedad' => 'Sociedad',
+                    'Economia'=>'Economia' , 'Deportes'=>'Deportes', 'Espectaculos'=>'Espectaculos', 'Opinion'=>'Opinion', 'Revista'=>'Revista',
+                    'Ciencia' => 'Ciencia', 'link1' => 'crearPublicacion', 'noticia' => 'Crear Noticia');
                 break;
             case '4':
-                $menu = array('link1' => 'publicacion','noticia' => 'Crear Noticia', 'link2' => 'administrador', 'administrador' => 'Administrar');
+                $menu = array('Coronavirus' => 'Coronavirus', 'Policiales' => 'Policiales', 'Politica' => 'Politica', 'Mundo' => 'Mundo',
+                    'Sociedad' => 'Sociedad', 'Ciencia' => 'Ciencia', 'link1' => 'publicacion',
+                    'Economia'=>'Economia' , 'Deportes'=>'Deportes', 'Espectaculos'=>'Espectaculos', 'Opinion'=>'Opinion', 'Revista'=>'Revista',
+                    'noticia' => 'Crear Noticia', 'link2' => 'administrador', 'administrador' => 'Administrar');
                 break;
             default:
-                $menu = array( 'Coronavirus' => 'Coronavirus', 'Policiales' => 'Policiales', 'Politica' => 'Politica', 'Mundo' => 'Mundo',
-                            'Sociedad' => 'Sociedad', 'Ciencia' => 'Ciencia');
+                $menu = array('Coronavirus' => 'Coronavirus', 'Policiales' => 'Policiales', 'Politica' => 'Politica', 'Mundo' => 'Mundo',
+                            'Sociedad' => 'Sociedad', 'Ciencia' => 'Ciencia',
+                    'Economia'=>'Economia' , 'Deportes'=>'Deportes', 'Espectaculos'=>'Espectaculos', 'Opinion'=>'Opinion', 'Revista'=>'Revista');
         }
+
 
         return $menu;
     }
